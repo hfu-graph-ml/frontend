934e9629e227810d68e3a44cf35fe4f3ffe26bd8
FelixSD <felix_98@outlook.de>
2022-04-21 16:20:14 +0200

    continued observer pattern

--- frontend/graphs/observe/DictHolder.py
+++ frontend/graphs/observe/DictHolder.py
@@ -1,3 +1,6 @@
+import asyncio
+import copy
+
 from frontend.graphs.data.BackendReader import BackendReader
 from frontend.graphs.observe.Observable import Observable
 from frontend.graphs.observe.Observer import Observer
@@ -5,11 +8,41 @@ from frontend.graphs.observe.Observer import Observer
 
 class DictHolder(Observable):
     _json_dict = []
+    # Dont touch that one during graph manipulation task
+    _dict_copy = []
     _observers = []
 
     def __init__(self, string_url: str):
         reader = BackendReader(string_url)
         self._json_dict = reader.deserialize_response()
+        self._dict_copy = copy.deepcopy(self._json_dict)
+
+        #self._dict_copy['graph']['nodes'][0]['name'] = "HHHHH"
+        self.async_loop()
+
+    def async_loop(self) -> None:
+        loop = asyncio.get_event_loop()
+        asyncio.ensure_future(self.monitor_dict())
+        loop.run_forever()
+
+    async def monitor_dict(self) -> None:
+        await self.check_dict()
+        print('t')
+        # TODO: continue
+
+    async def check_dict(self):
+
+        for key in self._json_dict['graph']:
+            for dict_entry in range(len(key)):
+                for entry_item in self._json_dict['graph'][key][dict_entry]:
+                    orig_val = self.get_orig_dict_entry_at(key,
+                                                           dict_entry,
+                                                           entry_item)
+                    copy_val = self.get_copy_dict_entry_at(key,
+                                                           dict_entry,
+                                                           entry_item)
+                    if orig_val != copy_val:
+                        self.notify_observer()
 
     def attach(self, observer: Observer) -> None:
         print('Observer added')
@@ -21,13 +54,10 @@ class DictHolder(Observable):
 
     # TODO: if changes are monitored, notify observer
     def notify_observer(self) -> None:
+        print('notified')
         for observer in self._observers:
             # TODO: on update, perform http post
             observer.update()
-
-    def monitor_dict(self) -> None:
-        print('t')
-        # TODO: continue
 
     def get_json_dict(self) -> dict:
         return self._json_dict
@@ -47,6 +77,13 @@ class DictHolder(Observable):
     def get_dict_edge_at(self, index: int) -> dict:
         return self._json_dict['graph']['edges'][index]
 
+    def get_orig_dict_entry_at(self, key: str, dict_idx: int, idx_entry):
+        return self._json_dict['graph'][key][dict_idx][idx_entry]
+
+    def get_copy_dict_entry_at(self, key: str, dict_idx: int, idx_entry):
+        return self._dict_copy['graph'][key][dict_idx][idx_entry]
+
 
 test = DictHolder('http://localhost:8000/graphs/')
+test.check_dict()
 print("t")
